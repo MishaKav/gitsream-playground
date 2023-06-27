c292e59a4924e3ad755b73b9ed672adbebfe6e1b
MishaKav <misha.kav@gmail.com>
2023-06-27 13:43:09 +0300

    testing format

--- testing_python.py
+++ testing_python.py
@@ -1,11 +1,13 @@
 from pydash import get
 
+
 def should_instrument(request, testing_functions_by_runtime):
-  event = get(obj,"event")
-  expected_event = get(obj,"expected")
-  expected_dismissed = get(obj,"dismissed",default={})
-  requested_runtimes = get(obj,"runtimes")
+  event = get(obj, "event")
+  expected_event = get(obj, "expected")
+  expected_dismissed = get(obj, "dismissed", default={})
+  requested_runtimes = get(obj, "runtimes")
   runtimes = [
-      testing_functions_by_runtime[runtime] for runtime in requested_runtimes  # type: ignore
+      # type: ignore
+      testing_functions_by_runtime[runtime] for runtime in requested_runtimes
   ]
-  yield ValidTestEvent(test_name, event, expected_event,expected_dismissed, runtim
+  yield ValidTestEvent(test_name, event, expected_event, expected_dismissed, runtim
