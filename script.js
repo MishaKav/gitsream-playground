1ef44cf2138ebe53469b584c37721f1d33e5f07c
Misha Kav <misha.kav@gmail.com>
2024-04-18 15:26:46 +0300

    test pure action 58

--- script.js
+++ script.js
@@ -2,24 +2,28 @@ const { RulesEngine } = require('@linearb/gitstream-core');
 // const { executeGitCommand } = require('@linearb/gitstream-core/dist/rule-engine/utils/git.service');
 const { execSync } = require('child_process');
 
-try {
-  const exec = cmd => {
-    try {
-      console.log(`Running command: ${cmd}`);
-      const res = execSync(cmd).toString();
-      console.log(res);
-      console.log('_________________________');
-    } catch (err) {
-      core.error(`Failed to exec: ${err}`);
-    }
-  };
+const main = async function () {
+  try {
+    const exec = cmd => {
+      try {
+        console.log(`Running command: ${cmd}`);
+        const res = execSync(cmd).toString();
+        console.log(res);
+        console.log('_________________________');
+      } catch (err) {
+        core.error(`Failed to exec: ${err}`);
+      }
+    };
 
-  exec('pwd');
-  exec('cd code/repo && ls && pwd');
-  exec('pwd');
-  console.log('_________________________________________________');
-  await RulesEngine().run();
-} catch (err) {
-  core.error(`Failed run RulesEngine: ${err}`);
-  process.exit(1);
-}
+    exec('pwd');
+    exec('cd code/repo && ls && pwd');
+    exec('pwd');
+    console.log('_________________________________________________');
+    await RulesEngine().run();
+  } catch (err) {
+    core.error(`Failed run RulesEngine: ${err}`);
+    process.exit(1);
+  }
+};
+
+main();
