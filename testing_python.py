2c5f8a2dd589661b499cb4a3ef247b83d939f34c
Misha Kav <misha.kav@gmail.com>
2024-04-21 14:56:17 +0300

    test pure action 91

--- testing_python.py
+++ testing_python.py
@@ -1,7 +1,7 @@
 from pydash import get
 
 def should_instrument(request, testing_functions_by_runtime):
-  event = get(obj,'event')
+  event = get(obj,"event")
   expected_event = get(obj,'expected')
   expected_dismissed = get(obj,"dismissed",default={})
   requested_runtimes = get(obj,"runtimes")
