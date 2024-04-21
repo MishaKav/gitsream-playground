a2b8036955d55d14da47b2ab3452becbe53be05e
Misha Kav <misha.kav@gmail.com>
2024-04-21 14:53:45 +0300

    revrt

--- testing_python.py
+++ testing_python.py
@@ -1,8 +1,8 @@
 from pydash import get
 
 def should_instrument(request, testing_functions_by_runtime):
-  event = get(obj,"event")
-  expected_event = get(obj,'expected') 
+  event = get(obj,'event')
+  expected_event = get(obj,'expected')
   expected_dismissed = get(obj,"dismissed",default={})
   requested_runtimes = get(obj,"runtimes")
   runtimes = [
