73e713b61004278acb73e3804e9d67573017e670
Misha Kav <misha.kav@gmail.com>
2024-08-18 16:21:16 +0300

    Update python_1.py

--- python_1.py
+++ python_1.py
@@ -1,4 +1,17 @@
 def say_hello():
     print('Hello, from Python')
+    print('Hello, from Python')
+    print('Hello, from Python')
     
 say_hello()
+
+def nop2():
+    print("test")
+    print("test")
+    print("test")
+    print("test")
+    print("test")
+    print("test")
+    print("test")
+    print("test")
+    print("test")
