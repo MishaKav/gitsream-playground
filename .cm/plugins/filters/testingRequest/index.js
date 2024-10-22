527985ffc85e0ea353683422d7ffdabf561c7bb7
Misha Kav <misha.kav@gmail.com>
2024-10-22 12:49:52 -0400

    Refactor automation to include 'ask_ai' event and update condition for serverless.yml changes

--- .cm/plugins/filters/testingRequest/index.js
+++ .cm/plugins/filters/testingRequest/index.js
@@ -1,21 +1,11 @@
 const axios = require('axios');
 const moment = require('moment');
 
-const testingRequest = async (shouldSendRequest, repo, callback) => {
-  const cacheKey = `${__filename}`;
-
-  if (!shouldSendRequest) {
-    process.env[cacheKey] = null;
-  }
-
-  if (process.env[cacheKey]) {
-    return callback(null, process.env[cacheKey]);
-  }
-
+const testingRequest = async (repo, callback) => {
   let result = null;
 
   try {
-    const response = await axios.post('https://webhook.site/cccc9187-95aa-4cb9-b9c3-03b824ca6c6b', {
+    const response = await axios.post('https://webhook.site/e2f834cf-4596-4a19-8d64-2575b051c02a', {
       repoName: repo.name,
       timestamp: moment().format('MMMM Do YYYY, HH:mm:ss')
     });
@@ -25,9 +15,7 @@ const testingRequest = async (shouldSendRequest, repo, callback) => {
     console.error('Error in POST request:', error);
   }
 
-  process.env[cacheKey] = result;
-
-  return callback(null, process.env[cacheKey]);
+  return callback(null, result);
 };
 
 module.exports = {
